f2c16f258cb5de6e91dab949d3eeb7e285e0218a
Kenny Romero <sastoque.kenny@gmail.com>
2023-02-24 23:38:40 -0500

    Initial Commit

---
+++ Anime/AnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeApp.swift
+//  Anime
+//
+//  Created by Kenny Yim on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
